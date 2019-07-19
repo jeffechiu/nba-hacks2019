869d33f4cee2e91eed3a78c89a809d7e42e450a9
Jeffrey Chiu <jchiu2@andrew.cmu.edu>
2019-07-19 12:45:56 +0800

    I should be working

--- Basketball Analytics/initialize.sql
+++ Basketball Analytics/initialize.sql
@@ -12,3 +12,7 @@ CREATE DATABASE Basketball;
 \copy play_by_play from 'Play_by_Play.csv' csv header
 
 \copy (SELECT * FROM Play_by_Play WHERE game_id='006728e4c10e957011e1f24878e6054a') to 'game_1_quarter_1.csv' with csv header
+
+\copy (SELECT DISTINCT team_id FROM Play_by_Play ORDER BY team_id) to 'teams.csv' with csv header
+
+\copy (SELECT DISTINCT person1 FROM Play_by_Play ORDER BY person1) to 'players.csv' with csv header
